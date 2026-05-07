2424a57c8849df4220348111a7df78f6c97e52d6
Ariel Weiss <arielweiss@roblox.com>
2026-05-07 16:06:20 -0700

    we need new stubs for codegen

--- wasm/src/codegen_stubs.cpp
+++ wasm/src/codegen_stubs.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Luau/Common.h"
+#include <cstdint>
 
 // Fast flags used by CodeGen - provide default values
 LUAU_FASTFLAGVARIABLE(DebugCodegenOptSize)
@@ -23,9 +24,26 @@ namespace CodeGen
 // (only used for AssemblyOptions::Host which we avoid in WASM)
 unsigned int getCpuFeaturesX64()
 {
-    // Return a reasonable set of features for assembly generation
-    // This doesn't affect actual execution since we're just generating text
     return 0;
+}
+
+// Defined in CodeGenContext.cpp which is excluded from the WASM build
+uint64_t jitRngSeed(uintptr_t ptr)
+{
+    uint64_t state = 0;
+    state = state * 6364136223846793005ULL + (105 | 1);
+    state += uint64_t(ptr);
+    state = state * 6364136223846793005ULL + (105 | 1);
+    return state;
+}
+
+uint32_t jitRngRandom(uint64_t& state)
+{
+    uint64_t oldstate = state;
+    state = oldstate * 6364136223846793005ULL + (105 | 1);
+    uint32_t xorshifted = uint32_t(((oldstate >> 18u) ^ oldstate) >> 27u);
+    uint32_t rot = uint32_t(oldstate >> 59u);
+    return (xorshifted >> rot) | (xorshifted << ((-int32_t(rot)) & 31));
 }
 
 } // namespace CodeGen
