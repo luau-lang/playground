63555c573c0ba2f9b383ad6baa807689a94fd8fe
Andrea Fletcher <50600470+BitwiseAndrea@users.noreply.github.com>
2026-07-08 15:50:37 -0700

    update flag

--- wasm/src/codegen_stubs.cpp
+++ wasm/src/codegen_stubs.cpp
@@ -13,7 +13,6 @@ LUAU_FASTINTVARIABLE(CodegenHeuristicsInstructionLimit, 25000)
 LUAU_FASTINTVARIABLE(CodegenHeuristicsBlockLimit, 25000)
 LUAU_FASTINTVARIABLE(CodegenHeuristicsBlockInstructionLimit, 25000)
 LUAU_FASTFLAGVARIABLE(LuauCodegenCounterSupport)
-LUAU_FASTFLAGVARIABLE(LuauCodegenInteger2)
 LUAU_FASTFLAGVARIABLE(LuauCodegenInteger3)
 
 namespace Luau
