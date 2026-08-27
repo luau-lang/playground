61bfb472c9e3038cd6efd32ea01726b89a91e482
ariel <arielweiss@roblox.com>
2026-08-27 11:59:34 -0700

    Fix the build again, notifications are working! (#78)
    We should be better going forward about noticing any breakage that
    arises here!

--- wasm/src/playground.cpp
+++ wasm/src/playground.cpp
@@ -41,7 +41,7 @@
 LUAU_FASTFLAG(LuauSolverV2)
 
 // this is defined in a unit we exclude in our build system, so I guess we can do this.
-// LUAU_FASTFLAGVARIABLE(LuauCodegenProtectData)
+LUAU_FASTFLAGVARIABLE(LuauCodegenProtectData)
 
 // Luau VM headers
 #include "lua.h"
